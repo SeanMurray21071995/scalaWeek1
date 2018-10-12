681fae85f1c5498574d42cada6217df34aed8c57
seanmurray21071995 <sean.murray@academytrainee.com>
2018-10-12 16:17:04 +0100

    Working on the intermediate questions Think im about to start the garage

--- src/main/scala/Intermediate.scala
+++ src/main/scala/Intermediate.scala
@@ -2,38 +2,59 @@ import scala.collection.mutable.ArrayBuffer
 
 object Intermediate extends App {
 
-  def blackJack(entry:Int, entry2:Int):Int=
-    {
-      if(entry >21 & entry2 >21){
-        0
-      }
-      if(21-entry > 21-entry2){
-
-        entry2
-      }
-      else if(21-entry<21-entry2)
-      {
-        entry
-      }
-      else{
-        0
-      }
+  def blackJack(entry: Int, entry2: Int): Int = {
+    if (entry > 21 & entry2 > 21) {
+      0
     }
+    if (21 - entry > 21 - entry2) {
+
+      entry2
+    }
+    else if (21 - entry < 21 - entry2) {
+      entry
+    }
+    else {
+      0
+    }
+  }
 
   //println(blackJack(12,10))
 
 
-
-  def uniquesSum(num1:Int, num2:Int, num3:Int):Int={
-    var arr = ArrayBuffer(num1,num2,num3)
+  def uniquesSum(num1: Int, num2: Int, num3: Int): Int = {
+    var arr = ArrayBuffer(num1, num2, num3)
     var dis = arr.distinct
     var check = arr diff dis
     var total = 0
-    for(a<-arr; b<-check){
-      if(a!=b | check.length==0)
-        total+= a
+    if (check.length != 0) {
+      for (a <- arr; b <- check) {
+        if (a != b)
+          total += a
       }
+    }
+    else {
+      for (a <- arr)
+        total += a
+    }
     total
   }
-  println(uniquesSum(12,8,4))
+
+  println(uniquesSum(12, 3, 8))
+
+  def totalHot(temp: Int, isSummer: Boolean): Boolean = {
+    var uperLimit = 90
+    var lowerLimit = 60
+    if (isSummer == true) {
+      uperLimit = 100
+      lowerLimit = 90
+    }
+    if (temp < uperLimit && temp > lowerLimit) {
+      true
+    }
+    else {
+      false
+    }
+  }
+
+  println(totalHot(123, true))
 }
